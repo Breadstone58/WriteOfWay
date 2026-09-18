3e036e87efd5bbe800f2d27579c3421238db5907
Breadstone58 <141655007+Breadstone58@users.noreply.github.com>
2026-09-18 19:43:59 -0400

    Revert "home screen button"
    This reverts commit bd0f5061077c5f1f8478cc480daed0e7c0a165ec.

--- Scenes/Levels/button.gd
+++ Scenes/Levels/button.gd
@@ -4,5 +4,10 @@ func _ready() -> void:
 	pass
 	
 func _process(_delta: float) -> void:
-	if Input.is_key_pressed(KEY_E) and get_overlapping_bodies().size() > 0:
+	pass
+	
+func _on_body_shape_entered(_body_rid: RID, _body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
+	if visible == true:
+		print("test")
+		if Input.is_key_pressed(KEY_E):
 				get_tree().change_scene_to_file("res://Scenes/Levels/level_0.tscn")
--- button.gd
+++ button.gd
@@ -1,7 +1,5 @@
 extends Area2D
 
-var ready_to_switch = false
-
 func _ready() -> void:
 	pass
 	
@@ -9,4 +7,7 @@ func _process(_delta: float) -> void:
 	pass
 	
 func _on_body_shape_entered(_body_rid: RID, _body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
-	pass
+	if visible == true:
+		print("test")
+		if Input.is_key_pressed(KEY_E):
+				get_tree().change_scene_to_file("res://Scenes/Levels/level_0.tscn")
