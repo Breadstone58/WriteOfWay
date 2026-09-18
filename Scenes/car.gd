5208061c18be2713ec4f1a898cfab2e6b75fd648
MJ7287 <mjwilcoxjp@gmail.com>
2026-09-18 09:03:56 -0400

    Movement updates + oil_line
    Added acceleration and friction and started implementation of oil_line functionality

--- Scenes/car.gd
+++ Scenes/car.gd
@@ -1,17 +1,38 @@
 extends CharacterBody2D
 
 
-@export var speed = 100     #how fast the character will move (pixels per sec)
-@export var rotation_speed = 1.5
+@export var max_speed = 200
+@export var rotation_speed = 2.5
+@export var acceleration = 600
+@export var friction = 500
 
 var rotation_direction = 0
+var oil_line = Line2D
 
+func _ready(): 
+	oil_line = Line2D.new()
+	oil_line.width = 6
+	oil_line.default_color = Color.BLACK
+	oil_line.antialiased = true
+	get_parent().add_child.call_deferred(oil_line)
 
 func get_input():
 	rotation_direction = Input.get_axis("turn_left", "turn_right")
-	velocity = transform.y * Input.get_axis("forward", "backward") * speed
+	var move_input = Input.get_axis("forward", "backward")
+	if move_input != 0:
+		var direction = transform.y.normalized()
+		velocity += direction * move_input * acceleration * get_physics_process_delta_time()
+		if velocity.length() > max_speed:
+			velocity = velocity.normalized() * max_speed
+	else: 
+		var speed = velocity.length()
+		speed -= friction * get_physics_process_delta_time()
+		speed = max(speed, 0)
+		velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
 	get_input()
-	rotation += rotation_direction * rotation_speed * delta * -1 * Input.get_axis("forward", "backward")
+	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
+	if Input.is_action_pressed("oil"):
+		oil_line.add_point(global_position)
