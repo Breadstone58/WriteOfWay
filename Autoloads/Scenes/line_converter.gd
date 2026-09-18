b102f843600ede77519cd418b1a5614463209f25
Breadstone58 <141655007+Breadstone58@users.noreply.github.com>
2026-09-18 15:37:02 -0400

    Image testing

--- Autoloads/Scenes/line_converter.gd
+++ Autoloads/Scenes/line_converter.gd
@@ -15,4 +15,4 @@ func _on_line_export(point_array: PackedVector2Array):
 	$Line2D.points = point_array
 	await RenderingServer.frame_post_draw
 	var image: Image = get_texture().get_image()
-	image.save_png("res://saved_image.png")
+	image.save_png("res://drawn_path.png")
