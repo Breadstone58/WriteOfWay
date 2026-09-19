fbd90fbf600422e39fec3bd3d6ab4f6809880e92
MJ7287 <mjwilcoxjp@gmail.com>
2026-09-18 23:24:57 -0400

    Added two levels, death animation, and music

--- Scenes/pedestrian.gd
+++ Scenes/pedestrian.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var anim: AnimatedSprite2D = $Anim
+var alive = true
 
 func _ready() -> void:
 	anim.animation = "walk"
@@ -10,9 +11,11 @@ func _process(_delta: float) -> void:
 	pass
 
 func _on_body_shape_entered(_body_rid: RID, _body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
-	if visible == true:
+	if alive == true:
+		alive = false
 		print("killed a guy")
+		anim.animation = "death"
+		anim.play()
+		anim.scale = Vector2(2,2)
+		$AnimationPlayer.speed_scale = 0
 		$AudioStreamPlayer2D.play()
-		visible = false
-		await $AudioStreamPlayer2D.finished
-		queue_free()
