4eb4c91a30fa4f81c4bf9e1348ac9b7deb81489b
FIREEMBLEMMASTER <39413942+FIREEMBLEMMASTER@users.noreply.github.com>
2026-09-19 18:07:12 -0400

    level 11 and 12

---
+++ Scenes/Levels/level_11_button.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+func _ready() -> void:
+	pass
+	
+func _process(_delta: float) -> void:
+	if Input.is_key_pressed(KEY_E) and get_overlapping_bodies().size() > 0:
+		get_tree().change_scene_to_file("res://Scenes/Levels/level_12.tscn")
+	
+func _on_body_shape_entered(_body_rid: RID, _body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
+	if visible == true:
+		print("test")
+		
