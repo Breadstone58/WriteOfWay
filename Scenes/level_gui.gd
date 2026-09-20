fe4768656194edb660ac1286d89e0f3a50b68f02
Breadstone58 <141655007+Breadstone58@users.noreply.github.com>
2026-09-19 22:06:40 -0400

    scrolls

--- Scenes/level_gui.gd
+++ Scenes/level_gui.gd
@@ -193,7 +193,11 @@ func display_rank():
 	$RankSFX.stream = rank_sfx[rank]
 	$RankAudio.play()
 	$RankSFX.play()
-	var level_num = int(String(get_tree().current_scene.name)[-1])
+	var level_num = 0
+	if String(get_tree().current_scene.name)[-1] == "_":
+		level_num = int(String(get_tree().current_scene.name)[-1])
+	else:
+		level_num = int(String(get_tree().current_scene.name)[-2]+String(get_tree().current_scene.name)[-1])
 	print("Level: " + str(level_num))
 	print("High_score: " + str(GlobVar.high_scores[level_num-1]))
 	if rank < GlobVar.high_scores[level_num-1]:
