281541eb2431675c8fc4bcba3cfdc6d751cc7418
Breadstone58 <141655007+Breadstone58@users.noreply.github.com>
2026-09-20 11:19:20 -0400

    credits

---
+++ Scenes/button_back.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_button_down() -> void:
+	SceneTransition.change_scene("res://Scenes/title_screen.tscn")
---
+++ Scenes/credits_button.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_button_down() -> void:
+	SceneTransition.change_scene("res://Scenes/credits_scene.tscn")
